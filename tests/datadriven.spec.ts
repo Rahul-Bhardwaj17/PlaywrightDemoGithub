c50173fabc5a52621b32e3d89fa45b154561c59e
Rahul Bhardwaj <kumar.raahul90@gmail.com>
2026-01-19 09:03:13 +0530

    QA-003: Remove expect from data driven spec file

--- tests/datadriven.spec.ts
+++ tests/datadriven.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import dataDriveSample from "../sampleData/dataDrivenSample.json";
 
 const users = dataDriveSample.data;
